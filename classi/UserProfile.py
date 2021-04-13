74598b6d4493a358b3e78e39d59be855b8b6725b
SingleSlice <castorina387@gmail.com>
2021-04-13 09:39:47 +0200

    userpfile

--- classi/UserProfile.py
+++ classi/UserProfile.py
@@ -27,14 +27,14 @@ class UserProfile :
     def saveUserDatabase(self) :
 
         print("dumped")
-        with open("database.pkl","wb") as d :
+        with open("cuteness count.pkl","wb") as d :
 
             pickle.dump(self.profili,d)
     
     def loadUserDatabase(self) :
 
         print("loaded")
-        with open("database.pkl","rb") as d :
+        with open("cuteness count.pkl","rb") as d :
 
             self.profili = pickle.load(d)
     
